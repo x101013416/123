36e72fd6a7afd46bc5f9ae4915b790e79a7f2294
x101013416 <76198048+x101013416@users.noreply.github.com>
2021-03-09 11:38:45 +0800

    Create txstockCOOKIE.js

---
+++ Task/txstockCOOKIE.js
@@ -0,0 +1,30 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let userheaderVal= ``
+
+let userkeyVal= ``
+
+let cashheaderVal= ``
+
+let signheaderVal= ``
+
+let signkeyVal= ``
+
+let taskheaderVal= ``
+
+let taskkeyVal= ``
+
+let wxtaskkeyVal= ``
+
+
+let cookieArr = {
+  userheaderVal: userheaderVal,
+  userkeyVal: userkeyVal,
+  cashheaderVal: cashheaderVal,
+  signheaderVal: signheaderVal,
+  signkeyVal: signkeyVal,
+  taskheaderVal: taskheaderVal, 
+  taskkeyVal: taskkeyVal,
+  wxtaskkeyVal: wxtaskkeyVal, 
+}
+
+module.exports =  cookieArr
