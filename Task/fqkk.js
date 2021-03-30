66f2cc497b02d502fd667d0d99c6443867163ee6
x101013416 <76198048+x101013416@users.noreply.github.com>
2021-03-30 20:12:01 +0800

    Update fqkk.js

--- Task/fqkk.js
+++ Task/fqkk.js
@@ -4,59 +4,34 @@
 脚本说明：番茄看看自动阅读
 脚本为自动完成番茄看看的阅读任务
 每日收益一元左右，可多号撸。提现秒到
-
 任务打开二维码地址 https://raw.githubusercontent.com/age174/-/main/3F545C70-389B-4155-ACB1-15B6FDA95501.jpeg
-
 可以去boxjs修改自动提现金额和循环次数
 最低提现额度为0.3元，默认提现1元
 最多循环次数为100次，默认为100次
-
 本脚本以学习为主！
 首次运行脚本，会提示获取数据
 去番茄看看，点击阅读A任务，开始阅读，
 完成一次阅读即可获取数据。
-
 TG电报群: https://t.me/hahaha8028
-
 我的邀请码 : 3950781  感谢大佬们填写
-
 注意:脚本每天运行一次就可以了，切记别多次运行，如不小心运行去把代理开关关闭一下就行，脚本默认运行次数为100次，运行时间大概15分钟,超过一百次容易被微信阅读限制，切记。。。。。。
 别几天就把羊薅死了，账号多的大佬觉得运行一次太久的话也可以去boxjs自行修改循环次数，比如修改循环次数为20，则每天运行五次脚本，循环次数为50则每天要运行两次脚本。。默认为循环次数为100一天运行一次，反正不管怎么修改，尽量每天循环次数别超过100
-
 boxjs地址 :  
-
 https://raw.githubusercontent.com/age174/-/main/feizao.box.json
-
-
 番茄看看
 圈X配置如下，其他软件自行测试
 [task_local]
 #番茄看看
 15 12 * * * https://raw.githubusercontent.com/age174/-/main/fqkk.js, tag=番茄看看, img-url=https://ftp.bmp.ovh/imgs/2021/02/f8306006536eb49c.jpeg, enabled=true
-
-
 [rewrite_local]
 #番茄看看
 ^http://m.*.top/reada/getTask url script-request-header https://raw.githubusercontent.com/age174/-/main/fqkk.js
-
-
-
 #loon
 ^http://m.*.top/reada/getTask script-path=https://raw.githubusercontent.com/age174/-/main/fqkk.js, requires-header=true, timeout=10, tag=番茄看看
-
-
-
 #surge
-
 番茄看看 = type=http-request,pattern=^http://m.*.top/reada/getTask,requires-header=1,max-size=0,script-path=https://raw.githubusercontent.com/age174/-/main/fqkk.js,script-update-interval=0
-
-
-
-
 [MITM]
 hostname = m.*.top
-
-
 */
 
 
@@ -68,7 +43,7 @@ let fqkkurl = $.getdata('fqkkurl')
 let fqkkhd = $.getdata('fqkkhd')
 let fqkey = ''
 let fqkkxh = ($.getval('fqkkxh') || '20');  // 此处修改循环次数，默认一百
-let fqtx = ($.getval('fqtx') || '100');  // 此处修改提现金额，0.1元等于10，默认为提现一元，也就是100
+let fqtx = ($.getval('fqtx') || '200');  // 此处修改提现金额，0.1元等于10，默认为提现一元，也就是100
 let max = 120
 let min = 37
 
